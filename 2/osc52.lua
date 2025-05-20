37ecd01511d60c18c9759bef32f5d869e39e9b6b
FredNica <frednica@FredNicas-MacBook-Air.local>
2025-05-20 22:49:24 +0800

    test

---
+++ 2/osc52.lua
@@ -0,0 +1,23 @@
+return {
+  "ojroques/nvim-osc52",
+  event = "VeryLazy",
+  config = function()
+    require("osc52").setup {
+      max_length = 0,          -- 0 = no limit
+      trim = true,
+      silent = false,
+      tmux = true,             -- Enable if using inside tmux
+    }
+
+    -- Auto yank to system clipboard on copy
+    local function copy()
+      if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+        require("osc52").copy_register("")
+      end
+    end
+
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      callback = copy,
+    })
+  end,
+}
